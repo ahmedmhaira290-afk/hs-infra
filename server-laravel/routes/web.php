13db5c896da477eac601c027adee4239a3cf044d
Ahmed <yourmail@gmail.com>
2026-06-25 18:29:54 +0100

    Use Route::redirect for root

--- server-laravel/routes/web.php
+++ server-laravel/routes/web.php
@@ -2,13 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    $path = public_path('index.html');
-    if (file_exists($path)) {
-        return response(file_get_contents($path), 200)->header('Content-Type', 'text/html');
-    }
-    return response('Not found', 404);
-});
+Route::redirect('/', '/index.html');
 
 Route::fallback(function () {
     if (str_starts_with(request()->path(), 'api/')) {
